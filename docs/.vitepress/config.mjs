019fa972f94cec9c0d30222517796dcb8624bbe7
地瓜球工程師 <joe22053814@gmail.com>
2024-12-30 16:20:25 +0800

    update config

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "vitepress";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/docs/",
   rewrites: {
     "/pages/(.*)": "/(.*)",
   },
