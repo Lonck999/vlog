03199354a7eb1c9b186cdefeb625afc67ceb3a9a
lonck <joe22053814@gmail.com>
2025-01-08 12:17:58 +0800

    Update 2025-01-08

--- docs/.vitepress/components/Resume.vue
+++ docs/.vitepress/components/Resume.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { ref, reactive } from "vue";
 
-const headImg = ref("/vlog/img/logo.png");
+const headImg = ref("/vlog/img/head.jpg");
 const name = ref("蕭鼎澄");
 const job = ref("Front-End Developer");
-const skills = reactive([
+const mainSkills = reactive([
   {
     id: 1,
     icon: "/vlog/img/vue-icon.svg",
@@ -19,15 +19,69 @@ const skills = reactive([
   },
   {
     id: 3,
+    icon: "/vlog/img/axios-icon.png",
+    name: "Axios",
+    class: "axios",
+  },
+  {
+    id: 4,
     icon: "/vlog/img/vueRouter-icon.png",
     name: "Vue-Router",
     class: "vueRouter",
   },
   {
-    id: 4,
+    id: 5,
     icon: "/vlog/img/vite-icon.svg",
     name: "Vite",
     class: "vite",
+  },
+]);
+
+const uiSkills = reactive([
+  {
+    id: 1,
+    icon: "/vlog/img/quasar-icon.svg",
+    name: "Quasar",
+    class: "quasar",
+  },
+  {
+    id: 2,
+    icon: "/vlog/img/veutify-icon.svg",
+    name: "Veutify",
+    class: "veutify",
+  },
+  {
+    id: 3,
+    icon: "/vlog/img/bootstrap-icon.svg",
+    name: "Bootstrap",
+    class: "bootstrap",
+  },
+  {
+    id: 4,
+    icon: "/vlog/img/element-icon.png",
+    name: "Element Plus",
+    class: "element",
+  },
+]);
+
+const cssSkills = reactive([
+  {
+    id: 1,
+    icon: "/vlog/img/tailwind-icon.svg",
+    name: "Tailwind",
+    class: "tailwind",
+  },
+  {
+    id: 2,
+    icon: "/vlog/img/scss-icon.svg",
+    name: "SCSS",
+    class: "scss",
+  },
+  {
+    id: 3,
+    icon: "/vlog/img/css-icon.svg",
+    name: "CSS 3",
+    class: "css",
   },
 ]);
 </script>
@@ -42,11 +96,30 @@ const skills = reactive([
       </div>
       <div class="skills">
         <p class="skills-title">Front-End Skills</p>
+        <p class="skills-main">前端主要：</p>
         <ul>
-          <li v-for="skill in skills" :key="skill.id">
-            <div :class="skill.class">
-              <img :src="skill.icon" :alt="skill.name" />
-              <p>{{ skill.name }}</p>
+          <li v-for="mainSkill in mainSkills" :key="mainSkill.id">
+            <div :class="mainSkill.class">
+              <img :src="mainSkill.icon" :alt="mainSkill.name" />
+              <p>{{ mainSkill.name }}</p>
+            </div>
+          </li>
+        </ul>
+        <p class="skills-ui">UI Framework：</p>
+        <ul>
+          <li v-for="uiSkill in uiSkills" :key="uiSkill.id">
+            <div :class="uiSkill.class">
+              <img :src="uiSkill.icon" :alt="uiSkill.name" />
+              <p>{{ uiSkill.name }}</p>
+            </div>
+          </li>
+        </ul>
+        <p class="skills-css">CSS類：</p>
+        <ul>
+          <li v-for="cssSkill in cssSkills" :key="cssSkill.id">
+            <div :class="cssSkill.class">
+              <img :src="cssSkill.icon" :alt="cssSkill.name" />
+              <p>{{ cssSkill.name }}</p>
             </div>
           </li>
         </ul>
@@ -69,6 +142,7 @@ const skills = reactive([
       border-radius: 50%;
       border: 0.4rem solid #efe9aa;
       padding: 0.6rem;
+      object-fit: cover;
     }
 
     .name-job {
@@ -101,11 +175,18 @@ const skills = reactive([
         margin: 1rem 0 0 0;
       }
 
+      .skills-main,
+      .skills-ui,
+      .skills-css {
+        font-size: 1.2rem;
+        color: #fff;
+        margin: 1rem 0;
+      }
+
       ul {
         display: flex;
         flex-wrap: wrap;
         gap: 0.5rem;
-        margin-top: 1rem;
 
         li {
           div {
@@ -135,12 +216,44 @@ const skills = reactive([
           border: 0.2rem solid #ffd700;
         }
 
+        .axios {
+          border: 0.2rem solid #5c21bb;
+        }
+
         .vueRouter {
           border: 0.2rem solid #328a63;
         }
 
         .vite {
           border: 0.2rem solid #9b5ffc;
+        }
+
+        .quasar {
+          border: 0.2rem solid #2f8aa6;
+        }
+
+        .veutify {
+          border: 0.2rem solid #5f9efc;
+        }
+
+        .bootstrap {
+          border: 0.2rem solid #563d7c;
+        }
+
+        .element {
+          border: 0.2rem solid #409eff;
+        }
+
+        .tailwind {
+          border: 0.2rem solid #3cadd0;
+        }
+
+        .scss {
+          border: 0.2rem solid #cd6799;
+        }
+
+        .css {
+          border: 0.2rem solid #3cadd0;
         }
       }
     }
